7e31d209436501d25095e9ae9eb4a1d5e947e4d0
Pratyush Vishal <pratyushvishal04@gmail.com>
2021-01-08 21:05:15 +0530

    Heroku deploy setup

--- server.js
+++ server.js
@@ -17,7 +17,15 @@ app.use('/api/tasks',require('./routes/api/tasks'));
 app.use('/api/user',require('./routes/api/user'));
 app.use('/api/auth',require('./routes/api/auth'));
 
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
 
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 //Express server
 const PORT = 5000 || process.env.PORT ;
