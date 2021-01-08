69c541c0a4efc292b823153b694a323344aa4571
Pratyush Vishal <pratyushvishal04@gmail.com>
2021-01-08 21:52:24 +0530

    Deploy Heroku

--- server.js
+++ server.js
@@ -17,17 +17,16 @@ app.use('/api/tasks',require('./routes/api/tasks'));
 app.use('/api/user',require('./routes/api/user'));
 app.use('/api/auth',require('./routes/api/auth'));
 
-// //Serve static assets if in production
-// if(process.env.NODE_ENV === 'production'){
-//     //Set static folder
-//     app.use(express.static('client/build'));
+//Serve static assets if in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'));
 
-//     app.get('*', (req,res) => {
-//         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-//     });
-// }
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
-// "heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
 
 //Express server
 const PORT = process.env.PORT || 5000 ;
