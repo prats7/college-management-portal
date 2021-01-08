a981b971cdab18b11da2f0df91ddfbf4aa37c601
Pratyush Vishal <pratyushvishal04@gmail.com>
2021-01-08 21:13:44 +0530

    Change port

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 //Express server
-const PORT = 5000 || process.env.PORT ;
+const PORT = process.env.PORT || 5000 ;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
