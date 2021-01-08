5c2fc3e60bbb97d4371ba19b6f86a2937406025f
prats7 <pratyushvishal04@gmail.com>
2021-01-08 19:29:37 +0530

    Add MongoDB

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+
+
+const app = express();
+
+//Bodyparser Middleware
+app.use(express.json());
+
+//Connect to db
+const db = require('./config/mongoose');
+
+//Use routes
+app.use('/api/tasks',require('./routes/api/tasks'));
+app.use('/api/user',require('./routes/api/user'));
+app.use('/api/auth',require('./routes/api/auth'));
+
+
+
+//Express server
+const PORT = 5000 || process.env.PORT ;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
